bf53f7350b1df7834573a04a6f475a0e6028a302
BradZzz <brad.zimmerman@episource.com>
2017-12-21 16:47:08 -0800

    init

--- app/config/Root.js
+++ app/config/Root.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import App from '../components/App';
+import Home from '../components/pages/Home/Home';
+import Sample from '../components/pages/Sample/Sample';
 
 const Root = () => {
   return (
     <Router>
-      <Route path="/" component={App} />
+      <div>
+        <Route path="/" component={ Home } />
+        <Route path="/sample" component={ Sample } />
+      </div>
     </Router>
   );
 };
